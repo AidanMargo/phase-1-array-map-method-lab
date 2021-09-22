854b852771e48ab5813a1520910f4d2a21182bb4
Aidan Margo <aidanmargo@gmail.com>
2021-09-22 15:29:16 -0500

    commit tree 18482ced278df7d71553c47155f5e7e65acef594

--- index.js
+++ index.js
@@ -12,5 +12,39 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+const updatedTutorials = tutorials.map(caseElement);
+return updatedTutorials;
 }
+
+
+const caseElement = word => {
+  let splitWords = word.split('');
+  let casedSentence = "";
+  for(word of splitWords){
+    word = word.charAt(0).toUpperCase() + word.slice(1);
+    casedSentence += word + ' '
+  } 
+  let removeEndSpace = casedSentence.slice(0, -1)
+  return removeEndSpace
+}
+
+console.log(caseElement(tutorials))
+
+
+
+
+
+
+//   let arr = string.map( s =>{
+//    return s.split(' ');
+//   })
+
+//   for(let i = 0; i < arr.length; i++){
+    
+//     return arr[i].map( word =>{
+//       return word.charAt(0).toUpperCase() + word.slice(1)
+//     }).join(' ');
+//   }
+//   return newArr
+// }
+// console.log(titleCased(tutorials))
